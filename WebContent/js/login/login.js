ff4f6d6ff4b8db398dca01b354c6dc56a72792e4
liaodong <991988207@qq.com>
2015-01-16 13:39:53 +0800

    add kaptcha to login

--- WebContent/js/login/login.js
+++ WebContent/js/login/login.js
@@ -1,3 +1,8 @@
 $(function() {
-    
+    /**
+     * kaptcha img change
+     */
+    $('#kaptcha').click(function() {
+        $(this).slideUp().attr('src', '../kaptcha.jpg?' + Math.floor(Math.random() * 100)).slideDown();
+    });
 });

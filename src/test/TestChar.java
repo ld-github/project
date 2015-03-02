f5d6605edf1a26384999644daf5c90f67ae09e64
liaodong <991988207@qq.com>
2015-03-02 14:27:58 +0800

    add TestChar.java

---
+++ src/test/TestChar.java
@@ -0,0 +1,29 @@
+package test;
+
+/**
+ * 
+ * <p>Title: TestVector</p>
+ * <p>Copyright: Copyright (c) 2015</p>
+ * <p>Description:</p>
+ *
+ * @author LD
+ *
+ * @date 2015-3-2
+ */
+public class TestChar {
+    public static void main(String[] args) {
+        char[] ch = new char[] { 'a', 'b', 'c' };
+        char[] aa = new char[26];
+        for (int i = 0; i < ch.length; i++) {
+            aa[ch[i] - 'a'] = 1;
+        }
+        String str = "";
+        for (int i = 0; i < 26; i++) {
+            if (aa[i] == 0) {
+                char c = (char) ('a' + i);
+                str = str + c;
+            }
+        }
+        System.out.print(str + "\t");
+    }
+}

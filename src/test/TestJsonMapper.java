538071791ce4ba00c7bea141b92e5923c3b577d2
liaodong <991988207@qq.com>
2015-08-07 02:39:13 +0800

    update TestJsonMapper.java

--- src/test/TestJsonMapper.java
+++ src/test/TestJsonMapper.java
@@ -6,38 +6,37 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.zhihuianxin.pojo.AccountCheck;
-import com.zhihuianxin.util.JsonMapper;
+import com.ld.web.bean.model.User;
+import com.ld.web.util.JsonMapper;
 
 public class TestJsonMapper {
 
     public static void main(String[] args) {
-        AccountCheck accountCheck = new AccountCheck();
-        accountCheck.setRequesttype("100");
-        String json = JsonMapper.getInstance().toJson(accountCheck);
+        User user = new User();
+        user.setUsername("100");
+        String json = JsonMapper.getInstance().toJson(user);
         System.out.println("json:" + json);
-        AccountCheck ac = JsonMapper.getInstance().toObject(json, AccountCheck.class);
-        ac.setVer("001");
-        System.out.println("ac.getRequesttype():" + ac.getRequesttype());
+        User u = JsonMapper.getInstance().toObject(json, User.class);
+        u.setPassword("001");
+        System.out.println("ac.getRequesttype():" + u.getUsername());
 
         Map<String, String> map = new HashMap<String, String>();
         map.put("1", "a");
         map.put("2", "3");
         System.out.println("map Json:" + JsonMapper.getInstance().toJson(map));
 
-        List<AccountCheck> list = new ArrayList<AccountCheck>();
-        list.add(ac);
-        list.add(accountCheck);
+        List<User> list = new ArrayList<User>();
+        list.add(u);
+        list.add(user);
 
         String listJson = JsonMapper.getInstance().toJson(list);
         System.out.println("listJson:" + listJson);
 
-        List<AccountCheck> listAccountCheck = JsonMapper.getInstance().toObject(listJson,
-                new TypeReference<List<AccountCheck>>() {
-                });
-        for (AccountCheck a : listAccountCheck) {
-            System.out.println(a.getRequesttype());
-            System.out.println(a.getVer());
+        List<User> listUser = JsonMapper.getInstance().toObject(listJson, new TypeReference<List<User>>() {
+        });
+        for (User lu : listUser) {
+            System.out.println(lu.getUsername());
+            System.out.println(lu.getPassword());
         }
     }
 

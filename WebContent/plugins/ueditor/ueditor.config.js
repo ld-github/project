0914e4f85ed4c3f01b3acf79b75494ecdbdd4d5b
liaodong <991988207@qq.com>
2015-10-27 17:52:48 +0800

    update config.js

--- WebContent/plugins/ueditor/ueditor.config.js
+++ WebContent/plugins/ueditor/ueditor.config.js
@@ -396,13 +396,9 @@
 
     function optimizationPath(path) {
         var protocol = /^[a-z]+:\/\//.exec(path)[0], tmp = null, res = [];
-
         path = path.replace(protocol, "").split("?")[0].split("#")[0];
-
         path = path.replace(/\\/g, '/').split(/\//);
-
         path[path.length - 1] = "";
-
         while (path.length) {
             if ((tmp = path.shift()) === "..") {
                 res.pop();
@@ -410,7 +406,6 @@
                 res.push(tmp);
             }
         }
-
         return protocol + res.join("/");
     }
 

9563726fbfd94688d9966bfbeebada16a8ac6970
liaodong <991988207@qq.com>
2015-10-28 17:05:06 +0800

    add FileManager.java

--- src/com/ld/web/util/FileManager.java
+++ src/com/ld/web/util/FileManager.java
@@ -1,9 +1,20 @@
 package com.ld.web.util;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ *<p>Title: FileManager</p>
+ *<p>Copyright: Copyright (c) 2015</p>
+ *<p>Description: 文件工具</p>
+ *
+ *@author LD
+ *
+ *@date 2015-10-28
+ */
 public class FileManager {
 
     private static final Logger logger = Logger.getLogger(FileManager.class);
@@ -18,8 +29,34 @@ public class FileManager {
         logger.info(String.format("Get suffix name by filename: %s", filename));
         String suffixName = filename.substring(filename.lastIndexOf("."));
 
-        logger.info(String.format("Get suffix name: %s", filename));
+        logger.info(String.format("Get suffix name: %s", suffixName));
         return suffixName;
+    }
+
+    /**
+     * Get prefixName by filename
+     * 
+     * @param filename
+     * @return
+     */
+    public static String getPrefixName(String filename) {
+        logger.info(String.format("Get prefix name by filename: %s", filename));
+        String prefixName = filename.substring(0, filename.lastIndexOf("."));
+
+        logger.info(String.format("Get prefix name: %s", prefixName));
+        return prefixName;
+    }
+
+    /**
+     * Make dirs by file directory
+     * 
+     * @param directory
+     */
+    public static void mkdirs(File directory) {
+        if (!directory.exists()) {
+            directory.mkdirs();
+            logger.info(String.format("Make dirs by filepath: %s", directory.getAbsoluteFile()));
+        }
     }
 
     /**
@@ -28,22 +65,61 @@ public class FileManager {
      * @param filepath
      */
     public static void mkdirs(String filepath) {
-        File folder = new File(filepath);
+        File directory = new File(filepath);
 
-        if (!folder.exists()) {
-            folder.mkdirs();
-            logger.info(String.format("Make dirs by filepath: %s", filepath));
+        mkdirs(directory);
+    }
+
+    /**
+     * Create File
+     * 
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    public static boolean createFile(File file) throws Exception {
+        File directory = file.getParentFile();
+
+        if (!directory.exists()) {
+            directory.mkdirs();
+            logger.info(String.format("Make dirs by filepath: %s", directory.getAbsoluteFile()));
         }
+
+        logger.info(String.format("Create file absolute path: %s ", file.getAbsolutePath()));
+        return file.createNewFile();
     }
 
-    public static void createFile(File file) {
-        String filepath = file.getParent();
-        mkdirs(filepath);
+    /**
+     * Create temp file
+     * 
+     * @param prefixName
+     * @param suffixName
+     * @param directory
+     * @return
+     * @throws IOException
+     */
+    public static File createTempFile(String prefixName, String suffixName, File directory) throws IOException {
+        if (null != directory) {
+            String path = directory.getAbsolutePath();
+            logger.info(String.format("Create temp file prefixName: %s, suffixName: %s, filepath: %s", prefixName, suffixName, path));
+            mkdirs(path);
+        }
+        File tempFile = File.createTempFile(prefixName, suffixName, directory);
+
+        logger.info(String.format("Create file absolute path: %s ", tempFile.getAbsolutePath()));
+        return tempFile;
     }
 
-    public static void main(String[] args) {
-        System.out.println(getSuffixName("123.jpg"));
-        mkdirs("d://adbv/asd");
-        createFile(new File("D://asd/asd/asd.jpg"));
+    /**
+     * Create temp file
+     * 
+     * @param filename
+     * @param directory
+     * @return
+     * @throws IOException
+     */
+    public static File createTempFile(String filename, File directory) throws IOException {
+        return createTempFile(getPrefixName(filename), getSuffixName(filename), directory);
     }
+
 }

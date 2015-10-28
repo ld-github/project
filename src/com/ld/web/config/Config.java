b6820d4c863f5a0810e0ac435a411d3dce948ff7
liaodong <991988207@qq.com>
2015-10-28 14:48:47 +0800

    update Config.js

--- src/com/ld/web/config/Config.java
+++ src/com/ld/web/config/Config.java
@@ -1,5 +1,15 @@
 package com.ld.web.config;
 
+/**
+ * 
+ *<p>Title: Config</p>
+ *<p>Copyright: Copyright (c) 2015</p>
+ *<p>Description: 系统配置</p>
+ *
+ *@author LD
+ *
+ *@date 2015-10-28
+ */
 public class Config {
 
 }

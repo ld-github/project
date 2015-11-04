1d7607226b75257d0495ed53ac1b6975dbf0cc4b
liaodong <991988207@qq.com>
2015-11-04 17:34:05 +0800

    update TestChar.jar

--- src/test/TestChar.java
+++ src/test/TestChar.java
@@ -12,18 +12,25 @@ package test;
  */
 public class TestChar {
     public static void main(String[] args) {
-        char[] ch = new char[] { 'a', 'b', 'c' };
-        char[] aa = new char[26];
-        for (int i = 0; i < ch.length; i++) {
-            aa[ch[i] - 'a'] = 1;
-        }
-        String str = "";
-        for (int i = 0; i < 26; i++) {
-            if (aa[i] == 0) {
-                char c = (char) ('a' + i);
-                str = str + c;
+        try {
+            char[] ch = new char[] { 'a', 'b', 'c' };
+            char[] aa = new char[26];
+            for (int i = 0; i < ch.length; i++) {
+                aa[ch[i] - 'a'] = 1;
             }
+            String str = "";
+            for (int i = 0; i < 26; i++) {
+                if (aa[i] == 0) {
+                    char c = (char) ('a' + i);
+                    str = str + c;
+                }
+            }
+            System.out.print(str + "\t");
+            throw new Exception("error");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
-        System.out.print(str + "\t");
     }
 }

2f8f9711c60b3fcd929e8b901b5b7fb704c6de55
liaodong <991988207@qq.com>
2015-11-06 15:35:09 +0800

    update ajaxListener

--- WebContent/js/global/ajaxListener.js
+++ WebContent/js/global/ajaxListener.js
@@ -20,12 +20,13 @@ $(function() {
      */
     $(document).ajaxError(function(event, request, settings) {
         $('#loading').hide();
-        new Message('加载错误...').show(false);
         if (request.status == 403) {
             new Message('用户登录已过期，请重新登录...').show(false);
             setTimeout(function() {
                 window.top.location.href = 'login.html';
             }, TIMEROUT_TO_LOGIN);
+        } else {
+            new Message('服务器错误或者链接超时...').show(false);
         }
     });
 });

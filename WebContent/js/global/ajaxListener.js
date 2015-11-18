e218420af0a91db72234a50cda310742f6d697ab
liaodong <991988207@qq.com>
2015-11-18 17:57:48 +0800

    update ajaxListener.js

--- WebContent/js/global/ajaxListener.js
+++ WebContent/js/global/ajaxListener.js
@@ -18,9 +18,9 @@ $(function() {
     /**
      * Ajax Error
      */
-    $(document).ajaxError(function(event, request, settings) {
+    $(document).ajaxError(function(event, response, settings) {
         $('#loading').hide();
-        if (request.status == 403) {
+        if (response.status == 403) {
             new Message('用户登录已过期，请重新登录...').show(false);
             setTimeout(function() {
                 window.top.location.href = 'login.html';
